e524313d5321021a7208676375c52dd444839692
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-02-26 17:19:00 -0600

    still messing around with some of the components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { TraceBeam } from "@/components/TraceBeam";
+import { BackBoxes } from "@/components/BackBoxes";
 import { MeteorCard } from "@/components/MeteorCard";
 export default function Home() {
   return (
-    <div>
+    <div className="">
+      {/* <BackBoxes/> */}
       <TraceBeam/>
     </div>
   );
